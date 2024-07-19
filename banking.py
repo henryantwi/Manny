517c24628b5b186e13693128dbb61096d3e01095
henryantwi <henryantwi191@gmail.com>
2024-07-19 11:26:26 +0000

    Sign In Done😊

--- banking.py
+++ banking.py
@@ -12,10 +12,40 @@ class Banking(Account):
 
     def sign_up(self):
         if self.create_client():
-            print("Account creation success")
+            print("Account creation success\nSign in with your credentials")
+            self.sign_in()
+        else:
+            print("Account creation unsuccessful!")
+        return self.home()
 
     def sign_in(self):
-        ...
+        if self.authenticate_client():
+            print("\n1. Transfer Money")
+            print("2. Make Withdrawal ")
+            print("3. Check Account Balance")
+            print("4. Deactivate Account")
+            print("5. Exit")
+            try:
+                choice = int(input("Enter choice: "))
+
+                if choice == 1:
+                    self.make_transfer()
+                elif choice == 2:
+                    self.make_withdraw()
+                elif choice == 3:
+                    self.check_balance()
+                elif choice == 4:
+                    self.deactivate()
+                elif choice == 5:
+                    exit()
+                else:
+                    print("Invalid Input")
+                    return self.sign_in()
+
+            except ValueError:
+                print("Invalid Input! Try Again")
+                self.sign_in()
+        self.sign_in()
 
     def make_deposit(self):
         ...
@@ -26,10 +56,19 @@ class Banking(Account):
     def make_transfer(self):
         ...
 
+    def check_balance(self):
+        ...
+
     def view_account_details(self):
         ...
+
+    def home(self):
+        pass
+
+    def deactivate(self):
+        pass
 
 
 if __name__ == "__main__":
     umat_bank = Banking()
-    umat_bank.sign_up()
+    umat_bank.sign_in()
--- sql_queries.py
+++ sql_queries.py
@@ -14,6 +14,7 @@ class Account:
     """
 
     def __init__(self) -> None:
+        self.account_number = None
         self.date_created = None
         self.phone_number = None
         self.address = None
@@ -35,21 +36,21 @@ class Account:
             print("There was a problem connecting to the database...")
             exit()
 
-    def create_client(self):
+    def create_client(self) -> bool:
         try:
-            self.name: str = input("\nEnter fullname: ")
+            self.name: str = (input("\nEnter fullname: ")).capitalize()
             self.age: str = input("Enter Age: ")
             self.address: str = input("Enter Address: ")
             self.phone_number: str = input("Enter phone number: ")
             self.date_created = datetime.now()
-            password: str = pin.pwinput("Enter your password: ", "🔑")
-            confirm_password: str = pin.pwinput("Confirm your password: ", "🔑")
+            password: str = pin.pwinput("Enter your password: ", "*")
+            confirm_password: str = pin.pwinput("Confirm your password: ", "*")
             if password == confirm_password:
                 self.client_password = bcrypt.hashpw(confirm_password.encode("utf-8"), bcrypt.gensalt())
             else:
                 print("Wrong Password!\nTry Again")
                 self.create_client()
-            self.username: str = f"@bk_{self.name.split(" ")[0].lower()}{randint(10, 100)}"
+            self.username: str = f"@cl_{self.name.split(" ")[0].lower()}_{randint(10, 100)}"
 
             cur = self.conn.cursor()
 
@@ -57,7 +58,7 @@ class Account:
                 INSERT INTO clients(name, age, address, balance, date_created, phone_number, user_name, password)
                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
             """
-            placeholders = (
+            placeholders: tuple = (
                 self.name,
                 self.age,
                 self.address,
@@ -75,10 +76,48 @@ class Account:
             cur.execute(f"SELECT acc_no FROM clients WHERE user_name = '{self.username}';")
 
             print("Account Details\n-------------\n")
-            print(f"Client Name: {self.name}\nUsername: {self.username}\nAccount_Number: {cur.fetchone()[0]}\nBalance: {self.balance}")
+            print(
+                f"Client Name: {self.name}\nUsername: {self.username}\nAccount_Number: {cur.fetchone()[0]}\nBalance: {self.balance}")
 
             return True
 
         except Exception as e:
             print(e)
             return False
+
+    def authenticate_client(self) -> bool:
+        username: str = input("Enter username: ")
+        password: str = pin.pwinput("Enter password: ", '*')
+
+        if username[0] != '@':
+            username = f"@{username}"
+
+        try:
+            cur = self.conn.cursor()
+            select_script = """
+                        SELECT password, acc_no, name, balance FROM clients
+                        WHERE user_name = %s;
+                    """
+            placeholder = (username,)
+
+            cur.execute(select_script, placeholder)
+            queryset = cur.fetchone()
+
+            stored_password = queryset[0]
+
+            if bcrypt.checkpw(password.encode("utf-8"), stored_password.encode("utf-8")):
+                self.account_number = queryset[1]
+                self.name = queryset[2]
+                self.username = username
+                self.balance = queryset[3]
+
+                print(f"\nLogin Successful....\n------------------------\nWelcome {self.name}")
+                return True
+            print("You entered a wrong password. Try Again🥺")
+            return False
+        except Exception as e:
+            print(f"There was an error: {e}")
+            return False
+
+
+
